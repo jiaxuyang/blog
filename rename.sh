6d3ee47b35dd4785829b505ecaf312b1f473a0c3
jiaxuyang <xuyang.jia@gmail.com>
2024-06-03 14:05:20 +0800

    feat: test
    Signed-off-by: jiaxuyang <xuyang.jia@gmail.com>

--- rename.sh
+++ rename.sh
@@ -11,4 +11,4 @@ if ( "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ) then
   export GIT_AUTHOR_NAME="$CORRECT_NAME"
   export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
-'--tag-name-filter cat --branches --tags
+' --tag-name-filter cat --branches --tags
