136c1fba7552c5268b2eab12cc27983f696e1a5c
jiaxuyang <xuyang.jia@gmail.com>
2024-06-03 14:00:04 +0800

    feat: test
    Signed-off-by: jiaxuyang <xuyang.jia@gmail.com>

---
+++ rename.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+git filter-branch --env-filter '
+OLD_EMAIL="jiaxuyang.i@dcarlife.com"
+CORRECT_NAME="jiaxuyang"
+CORRECT_EMAIL="xuyang.jia@gmail.com"
+if ( "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ) then
+  export GIT_COMMITTER_NAME="$CORRECT_NAME"
+  export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if ( "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ) then
+  export GIT_AUTHOR_NAME="$CORRECT_NAME"
+  export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+'--tag-name-filter cat --branches --tags
